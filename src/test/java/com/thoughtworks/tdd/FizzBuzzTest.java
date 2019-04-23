49cc806e374898ee812fdbc0c39d147e25fe7c73
zdazx <zhangduo0227@163.com>
2019-04-23 12:53:32 +0800

    TDD--test5

--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -40,6 +40,14 @@ public class FizzBuzzTest {
         assertThat(expectedResult, is(result));
     }
 
+    @Test
+    public void fizz_buzz_test5(){
+        int num = 15;
+        String expectedResult = "FizzBuzz";
+        String result = fizzBuzz.fizzBuzz(num);
+        assertThat(expectedResult, is(result));
+    }
+
 
 
 }
